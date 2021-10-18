d9cd8d0b483400741f4325da8719133419746f2a
Anton Cherepkov <anton.cherepkov@checklens.ai>
2021-10-18 19:41:15 +0000

    save all crops in one folder

--- data/crop.sh
+++ data/crop.sh
@@ -1,12 +1,6 @@
 python3 data/crop_pathes.py \
-    --names data/data/non_cropped/train.txt \
+    --names data/data/non_cropped/all_names.txt \
     --src_dataset_folder data/data/non_cropped \
-    --dst_dataset_folder data/data/cropped_train \
-    --window_size 512 \
-    --stride 408 && \
-python3 data/crop_pathes.py \
-    --names data/data/non_cropped/test.txt \
-    --src_dataset_folder data/data/non_cropped \
-    --dst_dataset_folder data/data/cropped_test \
+    --dst_dataset_folder data/data/cropped \
     --window_size 512 \
     --stride 408
