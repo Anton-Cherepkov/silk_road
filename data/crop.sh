89618c8cc429fb29db5ac66fd1b82232268e3997
Anton Cherepkov <anton.cherepkov@checklens.ai>
2021-10-18 19:08:09 +0000

    crop stride

--- data/crop.sh
+++ data/crop.sh
@@ -2,11 +2,11 @@ git statuspython3 data/crop_pathes.py \
     --names data/data/non_cropped/train.txt \
     --src_dataset_folder data/data/non_cropped \
     --dst_dataset_folder data/data/cropped_train \
-    --window_size 500 \
-    --stride 500 && \
+    --window_size 512 \
+    --stride 408 && \
 python3 data/crop_pathes.py \
     --names data/data/non_cropped/test.txt \
     --src_dataset_folder data/data/non_cropped \
     --dst_dataset_folder data/data/cropped_test \
-    --window_size 500 \
-    --stride 500
+    --window_size 512 \
+    --stride 408
