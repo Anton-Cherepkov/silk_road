16165fbbc1f6d92cd22294e33a51211407c5a6eb
Anton Cherepkov <anton.cherepkov@checklens.ai>
2021-10-19 10:38:30 +0000

    modify configs

--- configs/_base_/datasets/hackathon.py
+++ configs/_base_/datasets/hackathon.py
@@ -12,7 +12,7 @@ train_pipeline = [
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    # dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg']),
 ]
