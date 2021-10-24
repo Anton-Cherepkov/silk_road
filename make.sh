1de26a666bba43d276be323598a066dee3ec4e62
vitaly shubin <vitaly.shubin@checklens.ai>
2021-10-24 22:25:36 +0300

    one run cmd

--- make.sh
+++ make.sh
@@ -38,25 +38,9 @@ FP16_MODE=False
 run_web_ui() {
     log "Running WEB interface..."
     log "It will be available at http://localhost:${WEB_UI_PORT}"
-	  docker run \
-		  -e DISPLAY=unix${DISPLAY} \
-      -v /tmp/.X11-unix:/tmp/.X11-unix \
-      -v /etc/xdg/dvc:/etc/xdg/dvc:ro \
-		  --ipc=host \
-      --gpus all \
-      -itd \
-      --name=${CONTAINER_NAME}_web_ui \
-      -p ${WEB_UI_PORT}:8011 \
-      -v ${PWD}:${PROJECT_NAME} \
-      --entrypoint '/bin/sh' \
-      ${TAG_NAME} \
-      -c "python web_ui.py ${CONFIG} ${CHECKPOINT} --fp16 ${FP16_MODE}"
+	  docker exec ${CONTAINER_NAME} bash -c "python web_ui.py ${CONFIG} ${CHECKPOINT} --fp16 ${FP16_MODE}"
 }
 
-stop_web_ui() {
-    log "Stopping and removing the container ${CONTAINER_NAME}_web_ui"
-    docker stop ${CONTAINER_NAME}_web_ui; docker rm ${CONTAINER_NAME}_web_ui
-}
 # functions for run / stop web interface END
 
 # helper functions, used primarly for running traning START
